d5a5b559ce4c17e243ef09fabc414c2bcbd94d1c
Steve Tran <stran@redhat.com>
2019-08-14 16:11:53 -0400

    Testing vault integration

--- src/main/java/webapp/Example.java
+++ src/main/java/webapp/Example.java
@@ -4,16 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @RestController
 @EnableAutoConfiguration
+@EnableConfigurationProperties(MyConfiguration.class)
 public class Example {
+
+    private final MyConfiguration configuration;
+
+    public Example(MyConfiguration configuration) {
+        this.configuration = configuration;
+    }
 
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return String.format("Hello World %s %s !", configuration.getUsername(), configuration.getPassword());
     }
-    
+
     @RequestMapping("/health")
     public String healthz() {
     	return "status.:.UP";
