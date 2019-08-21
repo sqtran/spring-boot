3d6ce8525f0482e117b4840fc80b5804cddff7f8
Steve Tran <stran@redhat.com>
2019-08-21 13:55:52 -0400

    testing some stuff:

--- src/main/java/webapp/Example.java
+++ src/main/java/webapp/Example.java
@@ -5,14 +5,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.vault.annotation.VaultPropertySource;
+import org.springframework.beans.factory.annotation.Value;
+
 
 
 @RestController
 @EnableAutoConfiguration
 @EnableConfigurationProperties(MyConfiguration.class)
+@VaultPropertySource("secret/gs-vault-config")
 public class Example {
 
     private final MyConfiguration configuration;
+
+    @Value("username")
+    private String uname;
+
+    @Value("password")
+    private String pword;
 
     public Example(MyConfiguration configuration) {
         this.configuration = configuration;
@@ -20,7 +30,7 @@ public class Example {
 
     @RequestMapping("/")
     public String home() {
-        return String.format("Hello World %s %s !", configuration.getUsername(), configuration.getPassword());
+        return String.format("Hello World %s %s %s %s!", configuration.getUsername(), configuration.getPassword(), uname, pword );
     }
 
     @RequestMapping("/health")
