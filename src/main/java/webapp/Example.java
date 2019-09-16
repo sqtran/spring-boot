51d88d1bcffbbaa21dc01f7a432a6e2c75918f49
Steve Tran <stran@redhat.com>
2019-09-16 15:22:39 -0400

    Update message for better debugging

--- src/main/java/webapp/Example.java
+++ src/main/java/webapp/Example.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @EnableAutoConfiguration
@@ -11,8 +13,16 @@ public class Example {
 
     @RequestMapping("/")
     public String home() {
-        System.out.println("Home endpoint called!");
-        return "Hello World!";
+
+      String msg = "Hello World! ";
+      try {
+        System.out.println("Home endpoint called! " +  InetAddress.getLocalHost() + " : " + InetAddress.getLocalHost().getHostName());
+        msg += InetAddress.getLocalHost() + " : " + InetAddress.getLocalHost().getHostName();
+      } catch (UnknownHostException e) {
+          e.printStackTrace();
+      }
+
+        return msg;
     }
 
     @RequestMapping("/health")
